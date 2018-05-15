cda12f3ef75d1ae522aa50f5d37e125eeee45343
Mikko Luhtasaari <mikko.luhtasaari@gmail.com>
2018-05-15 14:46:34 +0300

    Add navigation button

--- src/components/AnsweredQuestions.js
+++ src/components/AnsweredQuestions.js
@@ -1,21 +1,33 @@
 import React, {Component} from "react"
 import {connect} from "react-redux"
 import {Link, withRouter} from "react-router-dom"
+import User from "./User"
 import Question from "./Question"
-    
+import Button from '@material-ui/core/Button';
+
 class AnsweredQuestions extends Component {
-    render(){
-        const {questions, authedUser} = this.props;
+
+    navigateToUnanswered = (e, id) => {
+        e.preventDefault();
+        this.props.history.push(`/unansweredquestions/${id}`)
+    };
+
+    navigate = itemProps => <Button
+        onClick={(e) => this.navigateToUnanswered(e, this.props.id)} {...itemProps} />;
+
+    render() {
+        const {questions, authedUser, id, users} = this.props;
+        const user = users[id];
 
         let answered = [];
         for (let key in questions) {
-            for(let i = 0; i < questions[key].optionOne["votes"].length; i++) {
-                if(questions[key].optionOne["votes"][i].toString().toLowerCase() === authedUser["id"].toString().toLowerCase()) {
+            for (let i = 0; i < questions[key].optionOne["votes"].length; i++) {
+                if (questions[key].optionOne["votes"][i].toString().toLowerCase() === authedUser["id"].toString().toLowerCase()) {
                     answered = answered.concat(questions[key])
                 }
             }
-            for(let i = 0; i < questions[key].optionTwo["votes"].length; i++) {
-                if(questions[key].optionTwo["votes"][i].toString().toLowerCase() === authedUser["id"].toString().toLowerCase()) {
+            for (let i = 0; i < questions[key].optionTwo["votes"].length; i++) {
+                if (questions[key].optionTwo["votes"][i].toString().toLowerCase() === authedUser["id"].toString().toLowerCase()) {
                     answered = answered.concat(questions[key])
                 }
             }
@@ -24,26 +36,38 @@ class AnsweredQuestions extends Component {
         console.log(answered);
         return (
             <div style={{paddingTop: 2 + "em"}} align="center">
-                <h2>AnsweredQuestions</h2>
-                <ul className="no-bullets">
-                {answered.map((question) => (
-                    <li key ={question["id"]}>
-                        <div>
-                            <Question id={question["id"]} isAnswered={true} />
-                        </div>
-                    </li>
-                ))}
-                </ul>
+                {user === undefined
+                    ? null
+                    : <div>
+                        <h1> Welcome back !</h1>
+                        <User id={id}/>
+                        <Button component={this.navigate} variant="raised">To Unanswered</Button>
+                        <h2>AnsweredQuestions</h2>
+                        <ul className="no-bullets">
+                            {answered.map((question) => (
+                                <li key={question["id"]}>
+                                    <div>
+                                        <Question id={question["id"]} isAnswered={true}/>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>}
             </div>
         )
     }
 }
 
-function mapStateToProps({questions, authedUser}) {
+function mapStateToProps({users, questions, authedUser}, props) {
+    const {id} = props.match.params;
+    const {user} = users[id];
     return {
+        id,
+        user,
+        authedUser,
         questions,
-        authedUser
+        users
     }
 }
-    
+
 export default withRouter(connect(mapStateToProps)(AnsweredQuestions))
