19292cbc7040d8096bb8d0808de10498e50ea2c3
Mikko Luhtasaari <mikko.luhtasaari@gmail.com>
2018-05-16 08:18:58 +0300

    Render unanswered questions

--- src/components/UnansweredQuestions.js
+++ src/components/UnansweredQuestions.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import {connect} from "react-redux"
 import {Link, withRouter} from "react-router-dom"
 import User from "./User"
+import Question from "./Question"
 import Button from '@material-ui/core/Button';
 
 class UnansweredQuestions extends Component {
@@ -14,10 +15,32 @@ class UnansweredQuestions extends Component {
     navigate = itemProps => <Button
         onClick={(e) => this.navigateToAnswered(e, this.props.id)} {...itemProps} />;
 
-    //    TODO get unanswered questions and remove to pass isAnswered when rendering questions
     render() {
-        const {id, users} = this.props;
+        const {id, users, authedUser, questions} = this.props;
         const user = users[id];
+
+        let unanswered = [];
+        for (let key in questions) {
+            let isAnswered = false;
+            for (let i = 0; i < questions[key].optionOne["votes"].length; i++) {
+                if (questions[key].optionOne["votes"][i].toString().toLowerCase() === authedUser["id"].toString().toLowerCase()) {
+                    isAnswered = true;
+                }
+            }
+            for (let i = 0; i < questions[key].optionTwo["votes"].length; i++) {
+                if (questions[key].optionTwo["votes"][i].toString().toLowerCase() === authedUser["id"].toString().toLowerCase()) {
+                    isAnswered = true;
+                }
+            }
+
+            if(!isAnswered) {
+                unanswered = unanswered.concat(questions[key]);
+            }
+        }
+        unanswered.sort((a, b) => b.timestamp - a.timestamp);
+        console.log(unanswered);
+
+
         return (
             <div align="center">
                 {user === undefined
@@ -27,7 +50,16 @@ class UnansweredQuestions extends Component {
                         <h1> Welcome back !</h1>
                         <User id={id}/>
                         <div><Button component={this.navigate} color="secondary" variant="raised">To Answered</Button>
-
+                            <h2>UnansweredQuestions</h2>
+                            <ul className="no-bullets">
+                                {unanswered.map((question) => (
+                                    <li key={question["id"]}>
+                                        <div>
+                                            <Question id={question["id"]} isAnswered={false}/>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </div>}
             </div>
